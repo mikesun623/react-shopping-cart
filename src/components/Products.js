9f5fb311c0fd60c1dba7b84303593071683a3a26
mike <sunmike1986@gmail.com>
2021-02-13 20:28:38 +1100

    commit 2

---
+++ src/components/Products.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'
+import formatCurrency from '../util'
+
+export default class Products extends Component {
+    render() {
+        return (
+            <div>
+                <ul className="productsfunc">
+                    {this.props.productsfunc.map(productobject=>(
+                        <li key={productobject._id}>
+                            <div className = "productcss">
+                                <a href={"#" + productobject._id}>
+                                    <img src={productobject.image} alt={productobject.title}></img>
+                                    <p>{productobject.title}</p>
+                                </a>
+                                <div className="product-price">
+                                    <div>{formatCurrency(productobject.price)}</div>
+                                    <button className="button primary">Add to Cart</button>
+                                </div>
+                            </div>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        )
+    }
+}
